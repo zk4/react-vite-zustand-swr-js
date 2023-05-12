200a1080ad4c7b5cbbc952befa4b92c5f4241a3e
zk <liuzq7@gmail.com>
2023-05-12 15:07:23 +0800

    chore: u

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,10 @@ import Root, {
 
 import EditContact, { action as editAction } from "./routes/edit";
 
+import { action as destroyAction } from "./routes/destroy";
+
+import Index from "./routes/index";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,6 +27,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+			{ index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
@@ -33,6 +38,11 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+			{
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+				errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,9 @@
-import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { Outlet,NavLink, Link, useLoaderData, Form ,redirect,useNavigation} from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export async function loader() {
@@ -12,7 +12,10 @@ export async function loader() {
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+
+	const { contacts } = useLoaderData();
+	const navigation = useNavigation();
+
 
   return (
     <>
@@ -39,7 +42,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+								<NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -48,7 +60,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -59,7 +71,11 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+			<div id="detail"
+			  className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+			>
         <Outlet />
       </div>
     </>
